f31d54f18f9b34589dbb890e916c729fdce594cd
Qudsia Fatima <fatima.munawarmohammedali@gmail.com>
2025-03-11 16:39:33 -0500

    Fix common runtime and logical errors in debug-common-errors.js

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,39 +24,40 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+
 
 // What’s Wrong?
-
+ console.log("Welcome to the bootcamp");
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
-for (let i = 0; i < numbers.length; i++) {
-  let doubled = numbers[i] * 2;
-  console.log(doubled);
-}
+
 
 // What’s Wrong?
-
+let numbers = [2, 4, "eight"];
+for (let i = 0; i< numbers.length; i++){
+  if (typeof numbers [i] === "number") {
+    console.log(numbers[i] * 2);
+} 
+}
 
 
 // Program C (Logic Error)
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
 
+
+// What’s Wrong?
 function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; 
 }
 
-console.log(isPrime(7)); // Expected true but gets false
-
-// What’s Wrong?
+console.log(isPrime(7));
